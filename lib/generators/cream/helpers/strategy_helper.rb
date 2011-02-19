ba6d14f5578c5733e98ef79df72255016c66e127
Sean McCullough <mcculloughsean@gmail.com>
2011-02-19 14:59:53 -0600

    fixed thing

--- lib/generators/cream/helpers/strategy_helper.rb
+++ lib/generators/cream/helpers/strategy_helper.rb
@@ -2,11 +2,11 @@ module Cream
   module GeneratorHelper
     module Strategy 
       def validate_strategy
-         if !valid_strategies.include?(strategy.to_sym)
-           say "ERROR: Strategy '#{strategy}' is not currently supported by Cream. Please use one of: #{valid_strategies_list}", :red
-           return false
-         end
-         true
+        if !valid_strategies.include?(strategy.to_sym)
+          say "ERROR: Strategy '#{strategy}' is not currently supported by Cream. Please use one of: #{valid_strategies_list}", :red
+          return false
+        end
+        true
       end
 
       def valid_strategies_list
@@ -14,17 +14,11 @@ module Cream
       end
 
       def valid_strategies
-<<<<<<< HEAD
-        @strategies ||= basic_strategies
-        @strategies |= document_store_strategies if document_store?
-        @strategies
-=======
         @strategies ||= begin
           s = basic_strategies          
           s |= document_store_strategies if document_store?
           s.flatten
         end
->>>>>>> 99cb2d49da7da3917369213365973fdfdded7b0e
       end
 
       def role_ref_strategy?
@@ -54,11 +48,11 @@ module Cream
       def inline_strategies
         valid_strategies - role_ref_strategies - role_embed_strategies
       end
-      
+
       def basic_strategies
         [:admin_flag, :role_string, :one_role, :many_roles, :roles_mask, :roles_string]
       end
-      
+
       def document_store_strategies 
         [:embed_one_role, :embed_many_roles, :role_strings]
       end
